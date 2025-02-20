3fbfcaeab5ade6b8b3c5803787b1121b9a04a1da
zimingmao-db <ziming.mao@databricks.com>
2025-02-20 00:01:31 -0800

    forgot reference

--- components/raftstore/src/store/fsm/apply.rs
+++ components/raftstore/src/store/fsm/apply.rs
@@ -2776,8 +2776,8 @@ where
             handle_region_split_with_old_guards(
                 old_guards.clone(),  // Pass a clone (or a reference if you can guarantee immutability)
                 new_region.get_id(),
-                new_start,
-                new_end
+                &new_start,
+                &new_end
             );
 
             // info!(
